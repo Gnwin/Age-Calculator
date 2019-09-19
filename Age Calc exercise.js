0dcd8fc40634acb443991ff4b957295ea0584ef7
Godwin Nwachukwu <nwgwin@gmail.com>
2019-09-19 12:18:38 +0100

    Add files via upload

---
+++ Age Calc exercise.js
@@ -0,0 +1,25 @@
+﻿let startButton = document.getElementById("start");
+let fillArea = document.getElementById("userinput");
+fillArea.style.display = "none";
+
+startButton.addEventListener("click", function() {
+  startButton.style.display = "none";
+  fillArea.style.display = "block";
+});
+
+const calcAge = () => {
+  let result;
+  let age = document.getElementById("agebox").value;
+  if (age < 0 || age == "" || isNaN(age)) {
+    window.alert(`You need positive integers to calculate days lived`);
+    return;
+  } else if (age > 200) {
+    window.alert(`Are you that old?`);
+    return;
+  } else {
+    age *= 365;
+    result = `You have lived ${age} days on Earth`;
+  }
+
+  document.getElementById("dayslived").innerHTML = result;
+};
